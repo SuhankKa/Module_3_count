6ad4a64167e4d3584d3ebe8193846506e408d794
Anastasiia <nastya21622162@gmail.com>
2024-12-11 20:50:21 +0300

    Add files via upload

--- Module_3_count.py
+++ Module_3_count.py
@@ -3,20 +3,20 @@
 calls = 0
 
 def count_calls():
-    print(calls) 
-    
-def string_info(string):
     global calls
+    calls += 1
+    return calls
+        
+def string_info(string):
+    count_calls()
     a = len(string)
     b = string.upper()
     c = string.lower()
     d = tuple([a, b, c])
-    calls += 1
     return d    
     
 def is_contains(string, list_to_search):
-    global calls
-    calls += 1  
+    count_calls()
     a = string.upper()
     b = "".join(list_to_search)
     c = b.upper()
@@ -27,5 +27,6 @@ print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
 print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
 print(calls)
 
